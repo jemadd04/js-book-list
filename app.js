2dd971c9394c972f02bfd5356dfe4c75781e4cfd
James Maddox <james.maddox07@gmail.com>
2018-08-12 21:11:28 -0400

    added function to delete books from list

--- app.js
+++ app.js
@@ -45,6 +45,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 }
 
+// Delete book
+UI.prototype.deleteBook = function(target) {
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 // Clear fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -52,7 +59,7 @@ UI.prototype.clearFields = function() {
   document.getElementById('isbn').value = '';
 }
 
-// Event Listeners
+// Event Listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
   // Get form values
   const title = document.getElementById('title').value,
@@ -80,7 +87,19 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
     ui.clearFields();
   }
 
+  e.preventDefault();
+});
 
+// Event listener for delete
+document.getElementById('book-list').addEventListener('click', function(e) {
+
+  // Instantiate UI
+  const ui = new UI();
+
+  ui.deleteBook(e.target);
+
+  // Show alert
+  ui.showAlert('Book Removed!', 'success');
 
   e.preventDefault();
 });
